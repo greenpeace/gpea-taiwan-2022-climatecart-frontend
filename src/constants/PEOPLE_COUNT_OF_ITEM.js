288e266b2e469b2cf64b41961d6c195744b580a7
bi-jen <bijen.chen@greenpeace.org>
2022-11-16 10:38:53 +0800

    update sighup people count

--- src/constants/PEOPLE_COUNT_OF_ITEM.js
+++ src/constants/PEOPLE_COUNT_OF_ITEM.js
@@ -1,72 +1,72 @@
 
 const PEOPLE_COUNT_OF_ITEM = {
     "1": {
-        value: 504,
+        value: 1225,
         type: "",
         message: "會思考的房子"
     },
     "2": {
-        value: 800,
+        value: 1597,
         type: "",
         message: "住得起的夢幻小屋"
     },
     "3": {
-        value: 452,
+        value: 770,
         type: "",
         message: "同等涼爽的夏天"
     },
     "4": {
-        value: 297,
+        value: 503,
         type: "",
         message: "再見了，加油站"
     },
     "5": {
-        value: 559,
+        value: 892,
         type: "",
         message: "低碳排的領域展開"
     },
     "6": {
-        value: 305,
+        value: 537,
         type: "",
         message: "坐得起的電動車"
     },
     "7": {
-        value: 406,
+        value: 710,
         type: "",
         message: "人人有光電"
     },
     "8": {
-        value: 352,
+        value: 583,
         type: "",
         message: "永續也能當飯吃"
     },
     "9": {
-        value: 496,
+        value: 1138,
         type: "",
         message: "出入平安"
     },
     "10": {
-        value: 240,
+        value: 431,
         type: "",
         message: "女力崛起"
     },
     "11": {
-        value: 325,
+        value: 619,
         type: "",
         message: "出一張嘴救地球"
     },
     "12": {
-        value: 837,
+        value: 565,
         type: "",
         message: "環保杯免煩惱"
     },
     "13": {
-        value: 520,
+        value: 881,
         type: "",
         message: "減塑領頭羊"
     },
     "14": {
-        value: 283,
+        value: 435,
         type: "",
         message: "海好有你"
     }
